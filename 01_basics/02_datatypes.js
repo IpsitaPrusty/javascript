6b384a93b23a0ee11210e4337bf6b80979cbeebe
Ipsita Prusty <145246053+IpsitaPrusty@users.noreply.github.com>
2024-02-05 12:31:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") we are using node.js, not browser
+
+//code reaadability
+let name = "ipsita";
+let age = 19;
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+//symbol => unique
+
+//object 
+
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefinded
