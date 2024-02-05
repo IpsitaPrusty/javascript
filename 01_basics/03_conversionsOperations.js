2eed07060fccf7b55825931438f5f87e6d10d1c2
Ipsita Prusty <145246053+IpsitaPrusty@users.noreply.github.com>
2024-02-05 12:53:11 +0000

    commit

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -18,3 +18,33 @@ console.log(m);//Nan
 //Nan => not a number
 //true => 1 and false => 0 and vice versa
 
+// ******************* Operations ***************
+let value = 3
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Ipsita"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);122
+console.log(1 + 2 + "2");//32
+
+console.log(true);//true
+console.log(+true);//1
+console.log(+"");//0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
