eab9417c76731552fccda51185369d1eebc686d0
Ipsita Prusty <145246053+IpsitaPrusty@users.noreply.github.com>
2024-02-05 13:14:03 +0000

    commit

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,16 @@
+//primitive
+
+// 7 types: String, Number, Boolean, Null , undefined, symbol, bigInt
+
+ 
+
+//reference (Non=primitive)
+//array,objects,functions
+let myObj = {
+    name: "ipi",
+    age:22,
+}//object
+
+const myFunction = function(){
+    console.log("hello");
+}
