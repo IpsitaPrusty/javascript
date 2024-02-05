8f941bc26242623c5ad36b027b9bb721fce39b7b
Ipsita Prusty <145246053+IpsitaPrusty@users.noreply.github.com>
2024-02-05 13:48:06 +0000

    commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "ipsita"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is {repoCount}`);
+
+const n = new String('ipsi')
+console.log(n[0]);
+console.log(n.__proto__);// we can see this using google console
+
+console.log(n.length);
+console.log(n.toUpperCase());
+console.log(n.charAt('2'));
+console.log(n.indexOf('p'));
+//substring and slice is also used
+
+ //replace and include- it checks whther that word is present or not  
