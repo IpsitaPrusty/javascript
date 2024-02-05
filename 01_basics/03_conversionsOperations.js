1cf0ae8e23787b7d473887e19705b1d47f688ac9
Ipsita Prusty <145246053+IpsitaPrusty@users.noreply.github.com>
2024-02-05 12:42:11 +0000

    conversions of data types

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,20 @@
+// let score = 33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let score = "33";
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+
+let s = "33abc";
+console.log(typeof s);
+let m = Number(s);
+console.log(typeof m);//number
+console.log(m);//Nan
+
+//Nan => not a number
+//true => 1 and false => 0 and vice versa
+
